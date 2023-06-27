a6fd165e5d8d4ddca5c279bc991cf531c06cccc1
sammanomar <>
2023-06-27 19:47:13 +0000

    add blogs details template

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('blogs/', blogs, name='blogs'),
     path('category_blogs/<str:slug>/', category_blogs, name='category_blogs'),
     path('tag_blogs/<str:slug>/', tag_blogs, name='tag_blogs'),
+    path('blog/<str:slug>/', blog_details, name='blog_details'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -86,3 +86,17 @@ def tag_blogs(request, slug):
         "all_blogs": all_blogs
     }
     return render(request, 'category_blogs.html', context)
+
+
+def blog_details(request, slug):
+    blog = get_object_or_404(Blog, slug=slug)
+    category = Category.objects.get(id=blog.category.id)
+    related_blogs = category.category_blogs.all()
+    tags = Tag.objects.order_by('-created_date')[:5]
+
+    context = {
+        "blog": blog,
+        "related_blogs": related_blogs,
+        "tags": tags
+    }
+    return render(request, 'blog_details.html', context)
