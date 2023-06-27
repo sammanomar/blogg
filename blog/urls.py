00e182c5eeec069ebfb6138d47bb4eba0ccad85f
sammanomar <>
2023-06-27 16:15:53 +0000

    add filtering blogs by category using a drop list

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,6 @@ from .views import *
 urlpatterns = [
     path('', home, name='home'),
     path('blogs/', blogs, name='blogs'),
+    path('category_blogs/<str:slug>/', category_blogs, name='category_blogs'),
+    path('tag_blogs/<str:slug>/', tag_blogs, name='tag_blogs'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.core.paginator import PageNotAnInteger, EmptyPage, Paginator
 
 from .models import (
     Blog,
+    Category,
     Tag
 )
 
@@ -37,3 +38,51 @@ def blogs(request):
         "paginator": paginator
     }
     return render(request, 'blogs.html', context)
+
+
+def category_blogs(request, slug):
+    category = get_object_or_404(Category, slug=slug)
+    queryset = category.category_blogs.all()
+    tags = Tag.objects.order_by('-created_date')[:5]
+    page = request.GET.get('page', 1)
+    paginator = Paginator(queryset, 2)
+    all_blogs = Blog.objects.order_by('-created_date')[:5]
+
+    try:
+        blogs = paginator.page(page)
+    except EmptyPage:
+        blogs = paginator.page(1)
+    except PageNotAnInteger:
+        blogs = paginator.page(1)
+        return redirect('blogs')
+
+    context = {
+        "blogs": blogs,
+        "tags": tags,
+        "all_blogs": all_blogs
+    }
+    return render(request, 'category_blogs.html', context)
+
+
+def tag_blogs(request, slug):
+    tag = get_object_or_404(Tag, slug=slug)
+    queryset = tag.tag_blogs.all()
+    tags = Tag.objects.order_by('-created_date')[:5]
+    page = request.GET.get('page', 1)
+    paginator = Paginator(queryset, 2)
+    all_blogs = Blog.objects.order_by('-created_date')[:5]
+
+    try:
+        blogs = paginator.page(page)
+    except EmptyPage:
+        blogs = paginator.page(1)
+    except PageNotAnInteger:
+        blogs = paginator.page(1)
+        return redirect('blogs')
+
+    context = {
+        "blogs": blogs,
+        "tags": tags,
+        "all_blogs": all_blogs
+    }
+    return render(request, 'category_blogs.html', context)
