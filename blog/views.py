1f4f49b6a427555094f0dc88e046d8a9f25dd9a1
sammanomar <>
2023-06-27 14:25:01 +0000

    view home page

--- blog/views.py
+++ blog/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render
 
 from .models import (
     Blog,
+    Tag
 )
 
 
 def home(request):
     blogs = Blog.objects.order_by('-created_date')
+    tags = Tag.objects.order_by('-created_date')
     context = {
-        "blogs": blogs
+        "blogs": blogs,
+        "tags": tags
     }
     return render(request, 'home.html', context)
 
