8a0d5caba225204e27ec71d86b30b3e44edc6fdf
sammanomar <>
2023-06-28 13:40:35 +0000

    finalize registration form

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from django.contrib import messages
+
+from .forms import (
+    UserRegistrationForm
+)
 
 # Create your views here.
 
@@ -8,4 +14,18 @@ def login_user(request):
 
 
 def register_user(request):
-    return render(request, 'registration.html')
+    form = UserRegistrationForm()
+
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data.get('password'))
+            user.save()
+            messages.success(request, "Registration sucessful")
+            return redirect('login')
+
+    context = {
+        "form": form
+    }
+    return render(request, 'registration.html', context)
