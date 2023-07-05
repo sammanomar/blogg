41ec960b1f080a08af82efe2541850ae19cb914b
sammanomar <sammanomar@hotmail.com>
2023-07-05 22:07:47 +0000

    update read me file

--- blog/forms.py
+++ blog/forms.py
@@ -2,14 +2,14 @@ from django import forms
 
 from .models import Blog
 
-
+from django_summernote.fields import SummernoteTextFormField, SummernoteTextField
 
 class TextForm(forms.Form):
     text = forms.CharField(widget=forms.Textarea, required=True)
 
 
 class AddBlogForm(forms.ModelForm):
-
+    description = SummernoteTextField
     
     class Meta:
         model = Blog
